767dc90e4d832eb57815e850a2d14c6de610f0d2
Kevin Earls <kearls@redhat.com>
2021-12-15 19:22:13 +0100

    After lecture 1
    Signed-off-by: Kevin Earls <kearls@redhat.com>

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3.0)
+            
+            Text("Hello, world!")
+
+        }
+        .padding(.horizontal)
+        .foregroundColor(Color.red)
+       
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
